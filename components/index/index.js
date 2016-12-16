a47ed7d05bd96855ef066702391ff25820312a0b
Ophidian-AC <contact@alonso.codes>
2016-12-15 22:25:09 -0500

    Redesigned landing page and login screen

--- components/index/index.js
+++ components/index/index.js
@@ -8,12 +8,20 @@ angular.module('app.index', ['ui.router'])
         .state('index', {
           url: '/',
           templateUrl: 'components/index/index.html',
-          controller: 'IndexController',
-          controllerAs: 'ic'
+          controller: 'LandingPage',
+          controllerAs: 'lp'
         })
 
 }])
 
-.controller('IndexController', [function() {
+.controller('LandingPage', ['$scope', function($scope) {
+
+  $scope.tabActive = 'signup';
+
+  //Toggle between Sign Up and Login tabs
+  $scope.tabSwitch = function (tab) {
+    $scope.tabActive = tab == 'signup' ? 'signup' : 'login';
+    // $scope.tabActive = !$scope.tabActive;
+  };
 
 }]);
